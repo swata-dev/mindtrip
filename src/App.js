83f9d187866d444cf36c38773526565fffac365e
lighter <zeke.kircheis@gmail.com>
2021-02-08 20:22:59 +0900

    create Photos.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Photos from "./Photos";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Photos />
     </div>
   );
 }
