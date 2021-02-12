361865bf7a5a285df8c711880eb1885d03fccb70
lighter <zeke.kircheis@gmail.com>
2021-02-12 17:35:42 +0900

    create env file
    環境変数設定用のファイルとモジュールの追加

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,15 @@
-import React , { useState } from 'react';
+import React , { useState,useEffect } from 'react';
 
 const Map = props => {
   const latitude = 34.997724;// 緯度
   const longitude = 135.913759;// 経度
+  const API_KEY = process.env.REACT_APP_MAP_API_KEY;
+
+  useEffect(()=>{
+    console.log(latitude);
+    console.log(longitude);
+    console.log(API_KEY);
+  })
   return (
     <section className="map">
       <p>image</p>
