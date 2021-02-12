6c986eb022d5af8f50c1f5e125590a51550e3d15
lighter <zeke.kircheis@gmail.com>
2021-02-12 17:14:16 +0900

    input sample data

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,8 @@
 import React , { useState } from 'react';
 
 const Map = props => {
+  const latitude = 34.997724;// 緯度
+  const longitude = 135.913759;// 経度
   return (
     <section className="map">
       <p>image</p>
