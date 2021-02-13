810cf5a7e278e16754241a2f5fa0bee73c4e0f37
lighter <zeke.kircheis@gmail.com>
2021-02-13 15:39:14 +0900

    apply google-map-react
    google-map-react のインストール

--- src/Map.js
+++ src/Map.js
@@ -1,23 +1,34 @@
-import React , { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div className="pin">{text}</div>;
 
 const Map = props => {
-  const latitude = 34.997724;// 緯度
-  const longitude = 135.913759;// 経度
-  const API_KEY = process.env.REACT_APP_MAP_API_KEY;
+  const [center,setCenter] = useState({
+    lat: 34.99,
+    lng: 135.91
+  });
+  const [zoom,setZoom] = useState(14);
+  const [api,setApi] = useState(process.env.REACT_APP_MAP_API_KEY);
 
   useEffect(()=>{
-    console.log(latitude);
-    console.log(longitude);
-    console.log(API_KEY);
   })
+
   return (
-    <section className="map">
-      <p>image</p>
-      <p>image</p>
-      <p>image</p>
-      <p>image</p>
-    </section>
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: api.slice(1,-2) }}
+        defaultCenter={center}
+        zoom={zoom}
+      >
+        <AnyReactComponent
+          lat={center.lat}
+          lng={center.lng}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
   );
 }
-
+ 
 export default Map;
